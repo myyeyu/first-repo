2b07314b9ffee7b301ffd7c1c620e843ce946061
zhangbolin <zhangbolin@cheerytech.ai>
2025-01-08 15:31:47 +0800

    1.添加一些注释

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import person.PersonFactory;
 public class Main {
   public static void main(String[] args) {
     // Person p1 = new Person("1001", "Leaf", 25);
+    // ...
     Person p1 = PersonFactory.getPerson();
     System.out.println("这是输出信息：" + p1);
   }
