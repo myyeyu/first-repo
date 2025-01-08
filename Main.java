f3922e9e4b677b6326437f7e41f1a7f4a745fb94
zhangbolin <zhangbolin@cheerytech.ai>
2025-01-08 14:34:18 +0800

    1.修改Main

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("This is the main class.");
+    Person p1 = new Person("1001", "Leaf", 25);
+    System.out.println("这是输出信息：" + p1);
   }
 }
